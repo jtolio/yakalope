3c90df10c2ed4a8b2e3c8327a6f09c981c29a31b
djanke <djanke@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-03-28 20:01:49 +0000

    Refactoring the trunk organization - Removing old directories

--- trunk/transports/pymsnt-0.11.3/src/legacy/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2004 James Bunton <james@delx.cjb.net>
-# Licensed for distribution under the GPL version 2, check COPYING for details
-
-from glue import LegacyConnection, LegacyGroupchat, translateAccount, startStats, updateStats, reloadConfig
-from glue import url, version, mangle, id, namespace
-from glue import formRegEntry, getAttributes, isGroupJID
