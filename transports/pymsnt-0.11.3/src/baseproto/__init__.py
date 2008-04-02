0a55c6715b151482ec7ff25137d0530676aca5eb
zfortier <zfortier@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-04-02 15:04:47 +0000

    importing the transports

---
+++ transports/pymsnt-0.11.3/src/baseproto/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2006 James Bunton <james@delx.cjb.net> 
+# Licensed for distribution under the GPL version 2, check COPYING for details
+
+from glue import LegacyConnection, LegacyGroupchat, translateAccount, startStats, updateStats, reloadConfig
+from glue import name, url, version, mangle, id, namespace
+from glue import formRegEntry, getAttributes, isGroupJID
