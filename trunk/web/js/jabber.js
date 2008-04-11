0fd897edf489a715ade8cd2868f686c6d7a5499e
uthornes <uthornes@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-04-11 21:02:56 +0000

    Added preliminary roster management

--- trunk/web/js/jabber.js
+++ trunk/web/js/jabber.js
@@ -1,11 +1,11 @@
-var con;
 var jabber = {
+  con: new Object(),
+  roster: new Array(),
   init: function() {
     oDbg = new JSJaCConsoleLogger(2);
 
     try { // try to resume a session
       con = new JSJaCHttpBindingConnection({'oDbg':oDbg});
-
       setupCon(con);
 
       if (con.resume()) {
@@ -18,18 +18,25 @@ var jabber = {
       con.disconnect();
   },
   
+  /**
+   * Registers handlers for XMPP stanzas
+   * @param {JSJaCHttpBindingConnection} con
+   */
   setupCon: function(con) {
-    con.registerHandler('message',jabber.handle.message);
-    con.registerHandler('presence',jabber.handle.presence);
-    con.registerHandler('iq',jabber.handle.iq);
-    con.registerHandler('onconnect',jabber.handle.connected);
-    con.registerHandler('onerror',jabber.handle.error);
-    con.registerHandler('status_changed',jabber.handle.statusChanged);
-    con.registerHandler('ondisconnect',jabber.handle.disconnected);
+    con.registerHandler('message', jabber.handle.message);
+    con.registerHandler('presence', jabber.handle.presence);
+    //con.registerHandler('iq', jabber.handle.iq);
+    con.registerHandler('onconnect', jabber.handle.connected);
+    con.registerHandler('onerror', jabber.handle.error);
+    con.registerHandler('status_changed', jabber.handle.statusChanged);
+    con.registerHandler('ondisconnect', jabber.handle.disconnected);
+		con.registerHandler('failure', jabber.handle.failure);
 
     con.registerIQGet('query', NS_VERSION, jabber.handle.iqVersion);
     con.registerIQGet('query', NS_TIME, jabber.handle.iqTime);
-    con.registerIQGet('query', NS_ROSTER, jabber.handle.iqRoster);
+    //con.registerIQGet('query', NS_ROSTER, jabber.handle.iqRosterGet);
+    con.registerIQSet('query', NS_ROSTER, jabber.handle.iqRosterSet);
+    con.registerHandler('iq', 'query', NS_ROSTER, jabber.handle.iqRoster);
   },
   
   doLogin: function(username, password) {
@@ -62,7 +69,11 @@ var jabber = {
       return false;
     }
   },
-  
+  /**
+   * Sends a message
+   * @param {JSJaCJID} user
+   * @param {String} msg
+   */
   sendMsg: function(user, msg) {
     try {
       var aMsg = new JSJaCMessage();
@@ -90,7 +101,7 @@ var jabber = {
     try {
       var roster = new JSJaCIQ();
       roster.setIQ(null, 'get', 'roster_1');
-      roster.setQuery('jabber:iq:roster');
+      roster.setQuery(NS_ROSTER);
       //roster.setFrom('');
       con.send(roster);
     } catch (e) {
@@ -115,6 +126,11 @@ var jabber = {
     this.__subscription(buddy, 'unsubscribed');
   },
   
+  /**
+   * Sends a subscription packet of a specified type
+   * @param {JSJaCJID} buddy
+   * @param {String} subType
+   */
   __subscription: function(buddy, subType) {
     try {
       var presence = new JSJaCPacket('presence');
@@ -175,11 +191,16 @@ var jabber = {
     
     connected: function() {
       con.send(new JSJaCPresence());
+      jabber.getRoster();
     },
     
     disconnected: function() {
         yakalope.app.clearBuddyList();
         Login.login();
+    },
+
+    failure: function(aJSJaCPacket) {
+      alert("Failure: " + aJSJaCPacket.xml());
     },
     
     iqVersion: function(iq) {
@@ -204,8 +225,85 @@ var jabber = {
     },
     
     iqRoster: function(iq) {
-      alert(iq.xml());
+	    try {
+				// Respond with 'result' packet
+	      var roster = new JSJaCIQ();
+	      roster.setIQ(null, 'result', iq.getID());
+	      roster.setQuery(NS_ROSTER);
+	      con.send(roster);
+				
+				var result = XMLTools.getXmlRecords(['group'], 'item', iq.getQuery());
+				
+				for(var i=0; i<result.records.length; i++) {
+					var node = result.records[i].node;
+					var jid = node.getAttribute('jid');
+					var name = node.getAttribute('name');
+					var subscription = node.getAttribute('subscription');
+					var group = result.records[i].data['group'];
+
+          yakalope.app.addBuddy(new Buddy(jid, subscription, name, group));
+				}
+      } catch (e) {
+	      alert("Error: " + e.message);
+	      return false;
+	    }
+    },
+    
+    iqRosterSet: function (iq) {
+      alert(iq.getQuery());
+			var record = iq.getQuery().firstChild;
+						
     },
   }    
 }
 
+/**
+ * Buddy type
+ * @param {String} jid
+ * @param {String} subscription Must be "none", "to", "from", "both"
+ * @param {String} name Nickname
+ * @param {String} group
+ */
+var Buddy = function (jid, subscription, name, group) {
+	this.jid = new JSJaCJID(jid);
+	this.subscription = subscription;
+	this.name = name;
+	this.group = group;
+}
+
+var XMLTools = {
+	/**
+	 * Simple map function. We use this instead of built-ins so we aren't
+	 * relying on too much javascript version complexity.
+	 * @param {Object} fun: Function to apply to the list
+	 * @param {Array} alist: List of things to apply the function to
+	 */
+	map: function(fun, alist){
+		var tmp = [];
+		for (var i = 0; i < alist.length; i++) 
+			tmp.push(fun(alist[i]));
+		return tmp;
+	},
+
+  /**
+   * Wrapper for the Ext.XMLReader which will generate a list of
+   * record objects with key-value pairings.
+   * @param {Array} tag_list: list of strings, will be the XML tags
+   * that are placed into a record object.
+   * @param {String} record_name: name of record that will appear in
+   * a series.
+   * @param {Object} xml_obj: the xml document to parse the data from
+   */
+  getXmlRecords: function(tag_list, record_name, xml_obj){
+  	tag_list = this.map(function(tag){
+  		return {
+  			name: tag
+  		};
+  	}, tag_list);
+  	var RecordObj = Ext.data.Record.create(tag_list);
+  	var readerObj = new Ext.data.XmlReader({
+  		record: record_name
+  	}, RecordObj);
+  	return readerObj.readRecords(xml_obj);
+  },
+}
