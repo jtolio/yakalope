3b2738802ecb370229595627d6b1dcabdc343ac8
zfortier <zfortier@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-04-11 18:57:04 +0000

    removing the stupid transports...
    git-svn-id: https://yakalope.googlecode.com/svn@50 02b2b53c-8f4b-0410-916d-4d5b64b87b60

--- trunk/jabber/transports/irc-transport-0.4/irc_helpers.py
+++
@@ -1,7 +0,0 @@
-# $Id: irc_helpers.py,v 1.1 2006/09/20 23:28:01 normanr Exp $
-
-_xlat = {91: u'{', 92: u'|', 93: u'}', 94: u'~'}
-def irc_ulower(str):
-    if str is None: return str
-    if len(str) == 0: return str
-    return str.translate(_xlat).lower()
--- trunk/jabber/transports/pyaim-t-0.8a/legacy/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2004-2006 Daniel Henninger <jadestorm@nc.rr.com>
-# Licensed for distribution under the GPL version 2, check COPYING for details
-
-from glue import LegacyConnection, LegacyGroupchat, translateAccount
-from glue import name, url, version, id, isGroupJID, defaultAvatar
