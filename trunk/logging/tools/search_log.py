ed48f5aa2f756a72a9d75da58c2682ab11018603
djanke <djanke@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-04-18 19:51:20 +0000

    Adding a tools directory.
    The search tool takes in index information then allows you
    to search the logs of a specific user using any valid
    Lucene query.
    
    
    git-svn-id: https://yakalope.googlecode.com/svn@79 02b2b53c-8f4b-0410-916d-4d5b64b87b60

---
+++ trunk/logging/tools/search_log.py
@@ -0,0 +1,30 @@
+import logmodule
+import sys
+
+if len(sys.argv) != 5:
+    print "Usage:",sys.argv[0],"[data directory] [index directory] [username] [query]"
+else:
+    #Prepare the logger module
+    logmod = logmodule.LogModule();
+    logmod.setDataDirectory(sys.argv[1])
+    logmod.setIndexDirectory(sys.argv[2])
+    
+    #Get a list of messages    print ' '
+    results = logger.searchMessages(sys.argv[3],sys.argv[4])
+    print len(results), ' results found'
+    for con in results:
+        print ""
+        print "==========================================================="
+        print "=    New Conversation                                     ="
+        print "==========================================================="
+        print "  Friend / Chat:", con.getFriendChat()
+        print "  Protocol:", con.getProtocol()
+        print "  Rank:", con.getRank()
+        print "-----------------------------------------------------------"
+        print "ID \t RANK \t TIME \t WHO SENT \t TEXT"
+        for msg in con.messages:
+            print msg.getID(),"\t",
+            print msg.getRank(),"\t",
+            print msg.getTimestamp(),"\t",
+            print msg.getWhoSent(),"\t",
+            print msg.getMessageText()
