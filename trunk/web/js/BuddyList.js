d71e56f9bf4f3cd25ff12e98f38ff1050cdf220d
uthornes <uthornes@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-04-21 03:41:49 +0000

    Buddy list and roster updates
    git-svn-id: https://yakalope.googlecode.com/svn@89 02b2b53c-8f4b-0410-916d-4d5b64b87b60

--- trunk/web/js/BuddyList.js
+++ trunk/web/js/BuddyList.js
@@ -1,6 +1,23 @@
 /*
  * Buddy List
  */
+var roster = [
+  ["test@example","b","c","Test Group","e","f"],
+  ["test@example","b","c","","e","f"]
+];
+var rosterStore = new Ext.data.GroupingStore({
+  reader: new Ext.data.ArrayReader({}, [
+    {name: 'jid'},
+    {name: 'subscription'},
+    {name: 'name'},
+    {name: 'group'},
+    {name: 'presence'},
+    {name: 'status'}
+  ]),
+  sortInfo: {field: 'jid', direction: "ASC"},
+  groupField: 'group'
+});
+
 BuddyList = Ext.extend(Ext.Panel, {
   id: 'buddylist',
   split: true,
@@ -27,192 +44,197 @@ BuddyList = Ext.extend(Ext.Panel, {
     return this.items.first();
   },
   addBuddy: function(buddy){
-  var friendRoot = this.getTreeRoot().firstChild;
-  if (!this.containsBuddy(buddy)) {
-    var newBuddy = new Ext.tree.AsyncTreeNode({
-    id: buddy.jid,
-    text: jid.toString(),
-    object: buddy,
-    iconCls: 'user',
-    leaf: true
-    });
-    newBuddy.on('click', this.onClick);
-    return friendRoot.appendChild(newBuddy);
+    var friendRoot = this.getTreeRoot().firstChild;
+    if (!this.containsBuddy(buddy)) {
+      var newBuddy = new Ext.tree.AsyncTreeNode({
+        id: buddy.jid,
+        text: jid.toString(),
+        object: buddy,
+        iconCls: 'user',
+        leaf: true
+      });
+      newBuddy.on('click', this.onClick);
+      return friendRoot.appendChild(newBuddy);
   }
   return null;
   },
   removeBuddy: function(buddy){
-  var tree = this.getTreeRoot();
-  var node = tree.firstChild.firstChild;
-  while (node != null) {
-    if (node.id.toString() == buddy.jid.toString()) {
-    node.remove();
-    break;
+    var tree = this.getTreeRoot();
+    var node = tree.firstChild.firstChild;
+    while (node != null) {
+      if (node.id.toString() == buddy.jid.toString()) {
+        node.remove();
+        break;
+      }
+      node = node.nextSibling;
     }
-    node = node.nextSibling;
-  }
   },
   clearBuddyList: function(){
-  var tree = this.getTreeRoot();
-  var node = tree.firstChild.firstChild;
-  while (node != null) {
-    nextnode = node.nextSibling;
-    node.remove();
-    node = nextnode;
-  }
+    var tree = this.getTreeRoot();
+    var node = tree.firstChild.firstChild;
+    while (node != null) {
+      nextnode = node.nextSibling;
+      node.remove();
+      node = nextnode;
+    }
   },
   containsBuddy: function(buddy){
-  var tree = this.getTreeRoot();
-  var node = tree.firstChild;
-  node = node.firstChild;
-  while (node != null) {
-    if (node.id.toString() == buddy.jid.toString()) {
-    return true;
+    var tree = this.getTreeRoot();
+    var node = tree.firstChild;
+    node = node.firstChild;
+    while (node != null) {
+      if (node.id.toString() == buddy.jid.toString()) {
+        return true;
+      }
+      else {
+        node = node.nextSibling;
+      }
     }
-    else {
-    node = node.nextSibling;
-    }
-  }
-  return false;
+    return false;
   },
   callRemoveBuddy: function(username){
-  var scope = yakalope.app.getBuddyList();
-  scope.removeBuddy(username);
+    var scope = yakalope.app.getBuddyList();
+    scope.removeBuddy(username);
   },
   addBuddyDlg: function(){
-  var servicesStore = new Ext.data.SimpleStore({
-    id: 'services-store',
-    fields: ['service', 'serviceName'],
-    data: [['squall.cs.umn.edu', 'squall.cs.umn.edu']    /*['yakalope.com', 'Yakalope'],
-     ['msn.yakalope.com', 'MSN'],
-     ['aim.yakalope.com', 'AIM'],
-     ['icq.yakalope.com', 'ICQ'],
-     ['irc.yakalope.com', 'IRC']*/
-    ],
-  });
-  var window = new Ext.Window({
-    title: 'Add a Buddy',
-    width: 300,
-    items: new Ext.FormPanel({
-    labelWidth: 50,
-    frame: true,
-    defaultType: 'textfield',
-    items: [{
-      fieldLabel: 'Name',
-      name: 'buddyname',
-      allowBlank: false,
-    }, new Ext.form.ComboBox({
-      id: 'serviceType',
-      name: 'serviceType',
-      fieldLabel: 'Service',
-      //hiddenName: 'serviceType_hidden',
-      value: servicesStore.getAt(0).data.service,
-      store: servicesStore,
-      valueField: 'service',
-      displayField: 'serviceName',
-      mode: 'local',
-      forceSelection: true,
-      triggerAction: 'all'
-    })],
-    }),
-  });
-  var form = window.items.first();
-  form.addButton({
-    text: 'Add',
-    name: 'add',
-  }, function(){
-    var values = form.getForm().getValues();
-    jabber.addBuddy(new Buddy(values.buddyname + '@' + values.serviceType, 'none'));
-    window.close();
-  }, window);
-  form.addButton({
-    text: 'Cancel',
-    name: 'cancel',
-  }, function(){
-    window.close();
-  }, window);
-  window.show(this);
+    var servicesStore = new Ext.data.SimpleStore({
+      id: 'services-store',
+      fields: ['service', 'serviceName'],
+      data: [['squall.cs.umn.edu', 'squall.cs.umn.edu']    /*['yakalope.com', 'Yakalope'],
+       ['msn.yakalope.com', 'MSN'],
+       ['aim.yakalope.com', 'AIM'],
+       ['icq.yakalope.com', 'ICQ'],
+       ['irc.yakalope.com', 'IRC']*/
+      ],
+    });
+    var window = new Ext.Window({
+      title: 'Add a Buddy',
+      width: 300,
+      items: new Ext.FormPanel({
+      labelWidth: 50,
+      frame: true,
+      defaultType: 'textfield',
+      items: [{
+        fieldLabel: 'Name',
+        name: 'buddyname',
+        allowBlank: false,
+      }, new Ext.form.ComboBox({
+        id: 'serviceType',
+        name: 'serviceType',
+        fieldLabel: 'Service',
+        //hiddenName: 'serviceType_hidden',
+        value: servicesStore.getAt(0).data.service,
+        store: servicesStore,
+        valueField: 'service',
+        displayField: 'serviceName',
+        mode: 'local',
+        forceSelection: true,
+        triggerAction: 'all'
+      })],
+      }),
+    });
+    var form = window.items.first();
+    form.addButton({
+      text: 'Add',
+      name: 'add',
+    }, function(){
+      var values = form.getForm().getValues();
+      jabber.addBuddy(new Buddy(values.buddyname + '@' + values.serviceType, 'none'));
+      window.close();
+    }, window);
+    form.addButton({
+      text: 'Cancel',
+      name: 'cancel',
+    }, function(){
+      window.close();
+    }, window);
+    window.show(this);
   },
   removeBuddyDlg: function(){
-  var window = new Ext.Window({
-    title: 'Remove a Buddy',
-    width: 300,
-    items: new Ext.FormPanel({
-    labelWidth: 75,
-    frame: true,
-    defaultType: 'textfield',
-    items: [{
-      fieldLabel: 'Buddy Name',
-      name: 'buddyname',
-      allowBlank: false,
-    }, {
-      fieldLabel: 'Service',
-      name: 'service',
-      allowBlank: false,
-    }],
-    }),
-  });
-  var form = window.items.first();
-  form.addButton({
-    text: 'Remove',
-    name: 'remove',
-  }, function(){
-    var values = form.getForm().getValues();
-    yakalope.app.getBuddyList().callRemoveBuddy(values.buddyname);
-    window.close();
-  }, window);
-  form.addButton({
-    text: 'Cancel',
-    name: 'cancel',
-  }, function(){
-    window.close();
-  }, window);
-  window.show(this);
+    var window = new Ext.Window({
+      title: 'Remove a Buddy',
+      width: 300,
+      items: new Ext.FormPanel({
+      labelWidth: 75,
+      frame: true,
+      defaultType: 'textfield',
+      items: [{
+        fieldLabel: 'Buddy Name',
+        name: 'buddyname',
+        allowBlank: false,
+      }, {
+        fieldLabel: 'Service',
+        name: 'service',
+        allowBlank: false,
+      }],
+      }),
+    });
+    var form = window.items.first();
+    form.addButton({
+      text: 'Remove',
+      name: 'remove',
+    }, function(){
+      var values = form.getForm().getValues();
+      yakalope.app.getBuddyList().callRemoveBuddy(values.buddyname);
+      window.close();
+    }, window);
+    form.addButton({
+      text: 'Cancel',
+      name: 'cancel',
+    }, function(){
+      window.close();
+    }, window);
+    window.show(this);
   },
   initComponent: function(){
-  Ext.apply(this, {
-    tbar: [{
-    text: 'Add Buddy',
-    id: 'addbuddy',
-    handler: this.addBuddyDlg,
-    scope: this
-    }, {
-    text: 'Remove Buddy',
-    id: 'removebuddy',
-    handler: this.removeBuddyDlg,
-    scope: this
-    }, {
-    text: 'Logout',
-    id: 'logout',
-    handler: function(){
-      jabber.quit()
-    },
-    scope: this
-    }],
-    items: [new Ext.tree.TreePanel({
-    id: 'im-tree',
-    title: 'Online Users',
-    loader: new Ext.tree.TreeLoader(),
-    rootVisible: false,
-    lines: false,
-    autoScroll: true,
-    root: new Ext.tree.AsyncTreeNode({
-      text: 'Online',
-      loaded: true,
-      children: [{
-      text: 'Friends',
-      expanded: true,
-      children: [],
+    Ext.apply(this, {
+      tbar: [{
+        text: 'Add Buddy',
+        id: 'addbuddy',
+        handler: this.addBuddyDlg,
+        scope: this
+      }, {
+        text: 'Remove Buddy',
+        id: 'removebuddy',
+        handler: this.removeBuddyDlg,
+        scope: this
+      }, {
+        text: 'Logout',
+        id: 'logout',
+        handler: function(){
+        jabber.quit()
+      },
+      scope: this
+      }],
+      items: [{
+          title: 'Buddies',
+          defaults: {border: false},
+          items: [ new Ext.grid.GridPanel({
+              store: rosterStore,
+              columns: [
+                {id: 'jid', dataIndex: 'jid'},
+                {dataIndex: 'group', hidden: true}
+              ],
+              view: new Ext.grid.GroupingView({
+                forceFit: true,
+                showGroupName: false,
+                groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Buddies" : "Buddy"]})'
+              }),
+              frame: false,
+              listeners: {
+                rowdblclick: function() {
+                    var jid = this.getSelectionModel().getSelected();
+                    yakalope.app.createNewChatWindow(jid);
+                }
+              }
+            })]
+        }, {
+        title: 'Settings',
+        html: '<p>Something userful would be in here.</p>',
       }]
-    })
-    }), {
-    title: 'Settings',
-    html: '<p>Something userful would be in here.</p>',
-    }]
-  });
-  BuddyList.superclass.initComponent.apply(this, arguments);
-  }
-  
+    });
+    BuddyList.superclass.initComponent.apply(this, arguments);
+  }  
 });
 
 Ext.reg('buddylist', BuddyList);
