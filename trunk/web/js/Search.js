18b31c2c18b44a1dfc02778035200875b6473748
zserre <zserre@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-04-25 21:14:33 +0000

    Added Search
    git-svn-id: https://yakalope.googlecode.com/svn@101 02b2b53c-8f4b-0410-916d-4d5b64b87b60

--- trunk/web/js/Search.js
+++ trunk/web/js/Search.js
@@ -1,173 +1,167 @@
 /**
  * @author serr0027
  */
-BuddyList = Ext.extend(Ext.Panel, {
-  id: 'buddylist',
-  split: true,
-  width: 225,
-  minSize: 150,
-  maxSize: 400,
-  //collapsible: true,
-  rootVisible: false,
-  lines: false,
-  layout: 'accordion',
-  defaults: {
-    border: false
-  },
-  layoutConfig: {
-    animate: true
-  },
-  onClick: function(node){
-    yakalope.app.createNewChatWindow(node.text);
-  },
-  getTreeRoot: function(){
-    return this.items.first().root;
-  },
-  getTreePane: function(){
-    return this.items.first();
-  },
-  addBuddyDlg: function(){
-    var servicesStore = new Ext.data.SimpleStore({
-      id: 'services-store',
-      fields: ['service', 'serviceName'],
-      data: [['squall.cs.umn.edu', 'squall.cs.umn.edu']    /*['yakalope.com', 'Yakalope'],
-       ['msn.yakalope.com', 'MSN'],
-       ['aim.yakalope.com', 'AIM'],
-       ['icq.yakalope.com', 'ICQ'],
-       ['irc.yakalope.com', 'IRC']*/
-      ],
-    });
-    var window = new Ext.Window({
-      title: 'Add a Buddy',
-      width: 300,
-      items: new Ext.FormPanel({
-      labelWidth: 50,
-      frame: true,
-      defaultType: 'textfield',
-      items: [{
-        fieldLabel: 'Name',
-        name: 'buddyname',
-        allowBlank: false,
-      }, new Ext.form.ComboBox({
-        id: 'serviceType',
-        name: 'serviceType',
-        fieldLabel: 'Service',
-        //hiddenName: 'serviceType_hidden',
-        value: servicesStore.getAt(0).data.service,
-        store: servicesStore,
-        valueField: 'service',
-        displayField: 'serviceName',
-        mode: 'local',
-        forceSelection: true,
-        triggerAction: 'all'
-      })],
-      }),
-    });
-    var form = window.items.first();
-    form.addButton({
-      text: 'Add',
-      name: 'add',
-    }, function(){
-      var values = form.getForm().getValues();
-      jabber.addBuddy(new Buddy(values.buddyname + '@' + values.serviceType, 'none'));
-      window.close();
-    }, window);
-    form.addButton({
-      text: 'Cancel',
-      name: 'cancel',
-    }, function(){
-      window.close();
-    }, window);
-    window.show(this);
-  },
-  removeBuddyDlg: function(){
-    var window = new Ext.Window({
-      title: 'Remove a Buddy',
-      width: 300,
-      items: new Ext.FormPanel({
-      labelWidth: 75,
-      frame: true,
-      defaultType: 'textfield',
-      items: [{
-        fieldLabel: 'Buddy Name',
-        name: 'buddyname',
-        allowBlank: false,
-      }, {
-        fieldLabel: 'Service',
-        name: 'service',
-        allowBlank: false,
-      }],
-      }),
-    });
-    var form = window.items.first();
-    form.addButton({
-      text: 'Remove',
-      name: 'remove',
-    }, function(){
-      var values = form.getForm().getValues();
-      yakalope.app.getBuddyList().callRemoveBuddy(values.buddyname);
-      window.close();
-    }, window);
-    form.addButton({
-      text: 'Cancel',
-      name: 'cancel',
-    }, function(){
-      window.close();
-    }, window);
-    window.show(this);
-  },
-  initComponent: function(){
-    Ext.apply(this, {
-      tbar: [{
-        text: 'Add Buddy',
-        id: 'addbuddy',
-        handler: this.addBuddyDlg,
-        scope: this
-      }, {
-        text: 'Remove Buddy',
-        id: 'removebuddy',
-        handler: this.removeBuddyDlg,
-        scope: this
-      }, {
-        text: 'Logout',
-        id: 'logout',
-        handler: function(){
-        jabber.quit()
-      },
-      scope: this
-      }],
-      items: [{
-          title: 'Buddies',
-          defaults: {border: false},
-          items: [ new Ext.grid.GridPanel({
-              store: rosterStore,
-              autoHeight: true,
-              columns: [
-                {id: 'jid', header: 'User', dataIndex: 'jid'},
-                {header: 'Group', dataIndex: 'group', hidden: true}
-              ],
-              view: new Ext.grid.GroupingView({
-                forceFit: true,
-                showGroupName: false,
-                groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Buddies" : "Buddy"]})'
-              }),
-              frame: false,
-              cls: 'blist-grid',
-              listeners: {
-                rowdblclick: function() {
-                    var buddy = this.getSelectionModel().getSelected().data;
-                    yakalope.app.createNewChatWindow(buddy.jid);
-                }
-              }
-            })]
-        }, {
-        title: 'Settings',
-        html: '<p>Something userful would be in here.</p>',
-      }]
-    });
-    BuddyList.superclass.initComponent.apply(this, arguments);
-  }  
+
+
+/*
+ * Ext JS Library 2.1
+ * 3 * Copyright(c) 2006-2008, Ext JS, LLC.
+ * licensing@extjs.com
+ *
+ * http://extjs.com/license
+ */
+
+FeedGrid = function(viewer, config) {
+ this.viewer = viewer;
+ Ext.apply(this, config);
+
+ this.store = new Ext.data.Store({
+   proxy: new Ext.data.HttpProxy({
+   url: 'feed-proxy.php'
+ }),
+ 
+ reader: new Ext.data.XmlReader(
+   {record: 'item'},
+   ['title', 'author', {name:'pubDate', type:'date'}, 'link', 'description', 'content']
+ )
+ });
+ this.store.setDefaultSort('pubDate', "DESC");
+
+ this.columns = [{
+   id: 'title',
+   header: "Title",
+   dataIndex: 'title',
+   sortable:true,
+   width: 420,
+   renderer: this.formatTitle
+ },{
+   header: "Author",
+   dataIndex: 'author',
+   width: 100,
+   hidden: true,
+   sortable:true
+ },{
+   id: 'last',
+   header: "Date",
+   dataIndex: 'pubDate',
+   width: 150,
+   renderer: this.formatDate,
+   sortable:true
+ }];
+ 
+ FeedGrid.superclass.constructor.call(this, {
+   region: 'west',
+   id: 'topic-grid',
+   loadMask: {msg:'Loading Feed...'},
+
+   sm: new Ext.grid.RowSelectionModel({
+   singleSelect:true
+ }),
+
+ viewConfig: {
+   forceFit:true,
+   enableRowBody:true,
+   showPreview:true,
+   getRowClass : this.applyRowClass
+ }
+ });
+
+ this.on('rowcontextmenu', this.onContextClick, this);
+};
+
+Ext.extend(FeedGrid, Ext.grid.GridPanel, {
+
+ onContextClick : function(grid, index, e){
+   if(!this.menu){ // create context menu on first right click
+     this.menu = new Ext.menu.Menu({
+     id:'grid-ctx',
+     items: [{
+     text: 'View in new tab',
+ iconCls: 'new-tab',
+ scope:this,
+ handler: function(){
+ this.viewer.openTab(this.ctxRecord);
+ }
+ },{
+ iconCls: 'new-win',
+ text: 'Go to Post',
+ scope:this,
+ handler: function(){
+ window.open(this.ctxRecord.data.link);
+ }
+ },'-',{
+ iconCls: 'refresh-icon',
+ text:'Refresh',
+ scope:this,
+ handler: function(){
+ this.ctxRow = null;
+ this.store.reload();
+ }
+ }]
+ });
+ this.menu.on('hide', this.onContextHide, this);
+ }
+ e.stopEvent();
+ if(this.ctxRow){
+ Ext.fly(this.ctxRow).removeClass('x-node-ctx');
+ this.ctxRow = null;
+ }
+ this.ctxRow = this.view.getRow(index);
+ this.ctxRecord = this.store.getAt(index);
+ Ext.fly(this.ctxRow).addClass('x-node-ctx');
+ this.menu.showAt(e.getXY());
+ },
+
+ onContextHide : function(){
+ if(this.ctxRow){
+ Ext.fly(this.ctxRow).removeClass('x-node-ctx');
+ this.ctxRow = null;
+ }
+ },
+
+ loadFeed : function(url) {
+ this.store.baseParams = {
+ feed: url
+ };
+ this.store.load();
+ },
+
+ togglePreview : function(show){
+ this.view.showPreview = show;
+ this.view.refresh();
+ },
+
+ // within this function "this" is actually the GridView
+ applyRowClass: function(record, rowIndex, p, ds) {
+ if (this.showPreview) {
+ var xf = Ext.util.Format;
+ p.body = '<p>' + xf.ellipsis(xf.stripTags(record.data.description), 200) + '</p>';
+ return 'x-grid3-row-expanded';
+ }
+ return 'x-grid3-row-collapsed';
+ },
+
+ formatDate : function(date) {
+ if (!date) {
+ return '';
+ }
+ var now = new Date();
+ var d = now.clearTime(true);
+ var notime = date.clearTime(true).getTime();
+ if (notime == d.getTime()) {
+ return 'Today ' + date.dateFormat('g:i a');
+ }
+ d = d.add('d', -6);
+ if (d.getTime() <= notime) {
+ return date.dateFormat('D g:i a');
+ }
+ return date.dateFormat('n/j g:i a');
+ },
+
+ formatTitle: function(value, p, record) {
+ return String.format(
+ '<div class="topic"><b>{0}</b><span class="author">{1}</span></div>',
+ value, record.data.author, record.id, record.data.forumid
+ );
+ }
 });
-
-Ext.reg('buddylist', BuddyList);
-
