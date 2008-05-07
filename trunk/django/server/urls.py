b59e1af19a7f42ebdc683b17d157e200342a5e0f
Derek.Janke <Derek.Janke@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-05-07 04:46:29 +0000

    Still much work to be done in terms of integration with the logging module and storing session information, BUT views are organized and it has been running on a local test server and returning JSON.
    git-svn-id: https://yakalope.googlecode.com/svn@119 02b2b53c-8f4b-0410-916d-4d5b64b87b60

--- trunk/django/server/urls.py
+++ trunk/django/server/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls.defaults import *
+from yakalope.views import login
+from yakalope.views import search
+from yakalope.views import recent
+from yakalope.views import logout
 
 urlpatterns = patterns('',
     # Example:
     # (r'^server/', include('server.foo.urls')),
+
+    (r'^login$', login),
+    (r'^search$', search),
+    (r'^recent$', recent),
+    (r'^logout$', logout),
 
     # Uncomment this for admin:
 #     (r'^admin/', include('django.contrib.admin.urls')),
