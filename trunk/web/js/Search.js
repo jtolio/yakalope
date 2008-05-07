22a6cdebbbfe6446f21089c23c9a687b3effc113
zserre <zserre@02b2b53c-8f4b-0410-916d-4d5b64b87b60>
2008-05-07 17:51:18 +0000

    more search edits

--- trunk/web/js/Search.js
+++ trunk/web/js/Search.js
@@ -2,6 +2,16 @@
  * Search
  */
 
+var store = new Ext.data.JsonStore({
+		url: 'http://mylocaldjangoserver/recent',
+		root: 'data',
+		id: 'id',
+		fields: ['id']
+	});
+	store.on('loadexception', function(proxy, store, response, e) {
+		alert('loadexception: ' + e.message);
+});
+ 
 LogWin = Ext.extend(Ext.Panel, {
   id: 'logwin',
   split: true,
@@ -18,56 +28,22 @@ LogWin = Ext.extend(Ext.Panel, {
   layoutConfig: {
     animate: true
   },
-  onClick: function(node){
-    yakalope.app.createNewChatWindow(node.text);
-  },
-  getTreeRoot: function(){
-    return this.items.first().root;
-  },
-  getTreePane: function(){
-    return this.items.first();
-  },
   initComponent: function(){
     Ext.apply(this, {
       tbar: [
 		 new Ext.app.SearchField({
 		 	id: 'search_field',
-			width: 100,
-            //store: store,
+			width: 150,
+            store: store,
             paramName: 'search'
-	  })],
-      items: [{
-          title: 'Search Results',
-          defaults: {border: false},
-          /*items: [ new Ext.grid.GridPanel({
-              store: rosterStore,
-              autoHeight: true,
-              columns: [
-               {id: 'jid', dataIndex: 'jid',
-                  renderer: function (value, p, record) {
-                    return String.format('{0}<br><span style="font-size:x-small;"><em>{1}</em>:{2}</span>',
-                      value, record.data.presence, record.data.status);
-                }},
-                {dataIndex: 'group', hidden: true},
-                {dataIndex: 'presence', hidden: true},
-                {dataIndex: 'subscription', hidden: true},
-                {dataIndex: 'status', hidden: true}
-              ],
-              view: new Ext.grid.GroupingView({
-                forceFit: true,
-                showGroupName: false,
-                groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Buddies" : "Buddy"]})'
-              }),
-              frame: false,
-              cls: 'blist-grid',
-              listeners: {
-                rowdblclick: function() {
-                    var buddy = this.getSelectionModel().getSelected().data;
-                    yakalope.app.createNewChatWindow(buddy.jid);
-                }
-              }
-            })]*/
-        }]
+	  }), {
+        text: 'Search',
+        id: 'search_button',
+        handler: function(){
+			alert(Ext.getCmp('search_field').getValue());
+		},
+        scope: this
+      }],
     });
     LogWin.superclass.initComponent.apply(this, arguments);
   }  
